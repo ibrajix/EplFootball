8bbbe10b4cde205bbaaf1777152d42c130c5fc06
ibrajix <ibrajix@gmail.com>
2021-12-22 22:41:28 +0100

    Added ContainerActivity.kt

---
+++ app/src/main/java/com/ibrajix/eplfootball/ui/activities/ContainerActivity.kt
@@ -0,0 +1,12 @@
+package com.ibrajix.eplfootball.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ibrajix.eplfootball.R
+
+class ContainerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_container)
+    }
+}
