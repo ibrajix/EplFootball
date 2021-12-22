551b65be22b76c9175cbe630ca51a987068ef1c4
ibrajix <ibrajix@gmail.com>
2021-12-23 00:40:17 +0100

    Finished Intro Screen

--- app/src/main/java/com/ibrajix/eplfootball/ui/activities/IntroActivity.kt
+++ app/src/main/java/com/ibrajix/eplfootball/ui/activities/IntroActivity.kt
@@ -2,11 +2,35 @@ package com.ibrajix.eplfootball.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import coil.load
 import com.ibrajix.eplfootball.R
+import com.ibrajix.eplfootball.databinding.ActivityIntroBinding
+import com.ibrajix.eplfootball.utils.Constants.IMAGE_URL
 
 class IntroActivity : AppCompatActivity() {
+
+    //initialize binding variable
+    private lateinit var binding: ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+
+        //assign binding variable
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+
+        //set content to view via binding
+        setContentView(binding.root)
+
+        //load my own code
+        initView()
+
     }
+
+    private fun initView(){
+        //display epl image with coil
+        binding.imageView.load(IMAGE_URL){
+            placeholder(R.drawable.ic_launcher_logo)
+        }
+    }
+
 }
--- app/src/main/java/com/ibrajix/eplfootball/utils/Constants.kt
+++ app/src/main/java/com/ibrajix/eplfootball/utils/Constants.kt
@@ -2,4 +2,5 @@ package com.ibrajix.eplfootball.utils
 
 object Constants {
     const val SPLASH_SCREEN_TIME = 1000L
+    const val IMAGE_URL = "https://user-images.githubusercontent.com/39574228/147166444-72050d06-5026-435e-9272-41c9ef59ceb9.png"
 }
